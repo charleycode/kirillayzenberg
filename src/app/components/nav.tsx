5968cfb864acdbfba48ee62c71e45534d370ae5e
Charles Park <charlespark4393@gmail.com>
2025-07-12 08:20:48 -0400

    test

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -27,7 +27,7 @@ export default function Nav() {
       {/* Desktop Menu */}
       <ul className="hidden md:flex gap-6">
         <li><Link href="/">Home <span className="hidden">Kirill Ayzenberg</span></Link></li>
-        <li><Link href="/about-kirk-ayzenberg">About <span className="hidden">Kirk Ayzenberg</span></Link></li>
+        <li><Link href="/about-kirill-kirk-ayzenberg">About <span className="hidden">Kirk Ayzenberg</span></Link></li>
         <li><Link href="/contact">Contact</Link></li>
       </ul>
 
@@ -47,7 +47,7 @@ export default function Nav() {
       {isOpen && (
         <ul className="absolute top-full left-0 w-full bg-white shadow-md md:hidden flex flex-col gap-4 p-4 z-50">
           <li><Link href="/">Home <span className="hidden">Kirill Ayzenberg</span></Link></li>
-          <li><Link href="/about-kirk-ayzenberg">About <span className="hidden">Kirk Ayzenberg</span></Link></li>
+          <li><Link href="/about-kirill-kirk-ayzenberg">About <span className="hidden">Kirk Ayzenberg</span></Link></li>
           <li><Link href="/contact">Contact</Link></li>
         </ul>
       )}
